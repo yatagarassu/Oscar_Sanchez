cb9dfc3161a5016cdecb8cdd47853f9432eef10f
Tony Morales <tonymorales101@gmail.com>
2020-03-21 15:55:58 -0600

    Cadena de Conexion ingresada

---
+++ CRUD/CRUD/CadConexion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+    class CadConexion
+    {
+        public string cadcon = "id de estación de trabajo = ParcialOscar.mssql.somee.com; tamaño del paquete = 4096; id de usuario = laxeous_SQLLogin_1; pwd = 57cl9aahy6; fuente de datos = ParcialOscar.mssql.somee.com; información de seguridad persistente = False; catálogo inicial = ParcialOscar";
+        //abre la conexion
+
+    }
+}
