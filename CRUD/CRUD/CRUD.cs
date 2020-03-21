5f0276eeb0d828371e5648c495f2b519b70d647e
Tony Morales <tonymorales101@gmail.com>
2020-03-21 15:28:38 -0600

    .

--- CRUD/CRUD/CRUD.cs
+++ CRUD/CRUD/CRUD.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CRUD
 {
-    public partial class Form1 : Form
+    public partial class CRUD : Form
     {
-        public Form1()
+        public CRUD()
         {
             InitializeComponent();
         }
